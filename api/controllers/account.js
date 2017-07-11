17fe9063c9f8a982cb308a288d765beca3622c5d
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-11 20:22:58 -0300

    Created Account controller structure close #11

---
+++ api/controllers/account.js
@@ -0,0 +1,19 @@
+import mongoose from 'mongoose';
+import helpers from '../helpers/helpers';
+const Account = mongoose.model('Account');
+
+module.exports.createAccount = function(req, res) {
+  
+}
+
+module.exports.returnAccount = function(req, res) {
+
+}
+
+module.exports.updateAccount = function(req, res) {
+
+}
+
+module.exports.deleteAccount = function(req, res) {
+
+}
