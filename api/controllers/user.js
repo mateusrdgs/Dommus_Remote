b2fbf1449b8802ba31fb7f9d743c273049f97425
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-11 21:06:02 -0300

    Created User controller structure close #12

---
+++ api/controllers/user.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import helpers from '../helpers/helpers';
+const User = mongoose.model('User');
+
+module.exports.createUser = function(req, res) {
+  
+}
+
+module.exports.returnUsers = function(req, res) {
+
+}
+
+module.exports.returnUserById = function(req, res) {
+
+}
+
+module.exports.updateUserById = function(req, res) {
+
+}
+
+module.exports.deleteUserById = function(req, res) {
+
+}
