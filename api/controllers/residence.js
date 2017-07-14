f466dc3a260c3590fe282016e4860d03c3cddb50
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-14 18:35:13 -0300

    Created Residence controller structure close #13

---
+++ api/controllers/residence.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import helpers from '../helpers/helpers';
+const Residence = mongoose.model('Residence');
+
+module.exports.createResidence = function(req, res) {
+  
+}
+
+module.exports.returnResidences = function(req, res) {
+
+}
+
+module.exports.returnResidenceById = function(req, res) {
+
+}
+
+module.exports.updateResidenceById = function(req, res) {
+
+}
+
+module.exports.deleteResidenceById = function(req, res) {
+
+}
