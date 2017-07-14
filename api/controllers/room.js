285488eddc03e33f657c45ba65bfa7f3664e3a2b
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-14 18:36:21 -0300

    Created Room controller structure close #14

---
+++ api/controllers/room.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import helpers from '../helpers/helpers';
+const Room = mongoose.model('Room');
+
+module.exports.createRoom = function(req, res) {
+  
+}
+
+module.exports.returnRooms = function(req, res) {
+
+}
+
+module.exports.returnRoomById = function(req, res) {
+
+}
+
+module.exports.updateRoomById = function(req, res) {
+
+}
+
+module.exports.deleteRoomById = function(req, res) {
+
+}
