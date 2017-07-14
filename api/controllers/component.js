b2a580d507cde9c81b0ec75fc9b11138308b1423
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-14 18:37:45 -0300

    Created Component controller structure close #15

---
+++ api/controllers/component.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+import helpers from '../helpers/helpers';
+const Component = mongoose.model('Component');
+
+module.exports.createComponent = function(req, res) {
+  
+}
+
+module.exports.returnComponents = function(req, res) {
+
+}
+
+module.exports.returnComponentById = function(req, res) {
+
+}
+
+module.exports.updateComponentById = function(req, res) {
+
+}
+
+module.exports.deleteComponentById = function(req, res) {
+
+}
