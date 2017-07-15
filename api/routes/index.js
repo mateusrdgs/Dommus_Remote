999423175c0c4b1d4d968f185869d0e4663377c4
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-15 17:52:05 -0300

    Created User routes close #20

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,11 +1,18 @@
 import express from 'express';
 import * as accountController from '../controllers/account';
+import * as userController from '../controllers/user';
 
 const router = express.Router();
 
-router.post('/account', accountController.createAccount);
 router.get('/account/:id', accountController.returnAccount);
+router.post('/account', accountController.createAccount);
 router.put('/account/:id', accountController.updateAccount);
 router.delete('/account/:id', accountController.deleteAccount);
+
+router.post('/account/:id/users/:id', userController.createUser);
+router.get('/account/:id/users', userController.returnUsers);
+router.get('/account/:id/users/:id', userController.returnUserById);
+router.put('/account/:id/users/:id', userController.updateUser);
+router.delete('/account/:id/users/:id', userController.deleteUser);
 
 export default router;
