e88659f9a447d1b64ce98f566eca99dfc57d1985
Mateus Rodrigues <devrdgs@gmail.com>
2017-07-16 20:20:15 -0300

    Created Residence routes close #22

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as accountController from '../controllers/account';
 import * as userController from '../controllers/user';
+import * as residenceController from '../controllers/residence';
 
 const router = express.Router();
 
@@ -14,5 +15,11 @@ router.get('/account/:id/users', userController.returnUsers);
 router.get('/account/:idAccount/users/:idUser', userController.returnUserById);
 router.put('/account/:idAccount/users/:idUser', userController.updateUserById);
 router.delete('/account/:idAccount/users/:idUser', userController.deleteUserById);
+
+router.post('/account/:idAccount/residences', residenceController.createResidence);
+router.get('/account/:idAccount/residences', residenceController.returnResidences);
+router.get('/account/:idAccount/residences/:idResidence', residenceController.returnResidenceById);
+router.put('/account/:idAccount/residences/:idResidence', residenceController.updateResidenceById);
+router.delete('/account/:idAccount/residences/:idResidence', residenceController.deleteResidenceById);
 
 export default router;
